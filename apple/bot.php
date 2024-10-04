9b5540fb2d6dadd48fcf0067404a3a1dbf1d6d16
FhFahim56 <appsdevelopera@gmail.com>
2024-10-04 16:14:05 +0600

    Update bot.php

--- apple/bot.php
+++ apple/bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "7846351090:AAE3g0GOM4YCxEU8Zom_Z8TscQ6GvKxsQ0g";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
